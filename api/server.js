3bc48febdc4c05f29caa6900400aac5cfe3b4fd2
nadiaxx <nadiasheikh4313@gmail.com>
2023-04-05 23:11:25 +0600

    mongo connection

--- api/server.js
+++ api/server.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb+srv://nadia:nadia@p300.g2wyq1k.mongodb.net/?retryWrites=true&w=majority', 
+{
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(console.error);
+
+
+
