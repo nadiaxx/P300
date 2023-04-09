d6de9d08ed97a0f49ece6917e1d9672efd6775ed
nadiaxx <nadiasheikh4313@gmail.com>
2023-04-10 00:10:55 +0600

    completed request

--- api/server.js
+++ api/server.js
@@ -24,14 +24,40 @@ app.get('/todos', async (req, res) => {
 	res.json(todos);
 });
 
+app.post('/todo/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
 
+	todo.save();
 
+	res.json(todo);
+});
 
+app.delete('/todo/delete/:id', async (req, res) => {
+	const result = await Todo.findByIdAndDelete(req.params.id);
 
+	res.json({result});
+});
 
+app.get('/todo/complete/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
 
+	todo.complete = !todo.complete;
 
+	todo.save();
 
+	res.json(todo);
+})
 
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
+});
 
 app.listen(3001, () => console.log("server started on port 3001"));
